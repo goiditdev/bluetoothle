bebbfc5e00aaf82eec976304915c01ad051269ad
Michael Hempel <mhempel.spam3@gmail.com>
2019-01-30 13:58:02 -0600

    changes for testing

--- Samples/Samples.iOS/AppDelegate.cs
+++ Samples/Samples.iOS/AppDelegate.cs
@@ -3,6 +3,8 @@ using Foundation;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
+using Plugin.BluetoothLE;
+using CoreFoundation;
 
 
 namespace Samples.iOS
@@ -13,8 +15,24 @@ namespace Samples.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             Forms.Init();
+
+            BleAdapterConfiguration adapterconfig = new BleAdapterConfiguration();
+            adapterconfig.ShowPowerAlert = true;
+            adapterconfig.DispatchQueue = new CoreFoundation.DispatchQueue("acrble");
+
+            adapterconfig.RestoreIdentifier = "pluginbluetoothle";//fails when uncommented
+            //adapterconfig.RestoreIdentifier = "acr";              //works fine
+            //adapterconfig.RestoreIdentifier = "acr2";             //works fine
+            //adapterconfig.RestoreIdentifier = "pluginbluetoothl"; //works fine
+            //adapterconfig.RestoreIdentifier = "pluginbluetoothle2"; //works fine
+            //adapterconfig.RestoreIdentifier = "pluginbluetoothl2"; //works fine
+
+
+            CrossBleAdapter.Init(adapterconfig);
+
             this.LoadApplication(new App(new PlatformInitializer()));
             new Acr.XamForms.ItemTappedCommandBehavior();
+
             //UIApplication.SharedApplication.IdleTimerDisabled = false;
             return base.FinishedLaunching(app, options);
         }
